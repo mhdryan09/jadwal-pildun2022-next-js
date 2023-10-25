a278bf8b56b4717b311b4d4c90ceca9ad9a1eb6b
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-10-25 17:51:14 +0700

    Section List : Fetch API List Match

--- app/section/sectionList.tsx
+++ app/section/sectionList.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
-export default function SectionList() {
-  return (
-    <div>sectionList</div>
-  )
+// fetch data - get matches
+const getMatches = async () => {
+	const response = await fetch("https://worldcupjson.net/matches");
+	return response.json();
+};
+
+export default async function SectionList() {
+	const matches: Match[] = await getMatches();
+
+	return (
+		<div>
+			{matches.map((match, index) => (
+				<div key={index}>
+          <p>{match.datetime}</p>
+          {match.home_team_country} vs {match.away_team_country}
+        </div>
+			))}
+		</div>
+	);
 }
