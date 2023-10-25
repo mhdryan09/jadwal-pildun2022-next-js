ac37cbb692cdf6c9eb2438401b66858fa9026949
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-10-25 18:07:38 +0700

    List Match Per Tangal

--- app/section/sectionList.tsx
+++ app/section/sectionList.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { groupBy } from "lodash";
+import moment from "moment";
 
 // fetch data - get matches
 const getMatches = async () => {
@@ -8,15 +10,27 @@ const getMatches = async () => {
 
 export default async function SectionList() {
 	const matches: Match[] = await getMatches();
+  // ambil format tanggal
+  const getMatchDate = (item:Match) => moment(item.datetime)
+  .format('YYYY-MM-DD')
+
+  const matchesByDate = groupBy(matches, getMatchDate);
 
 	return (
 		<div>
-			{matches.map((match, index) => (
+			{/* {matches.map((match, index) => (
 				<div key={index}>
           <p>{match.datetime}</p>
           {match.home_team_country} vs {match.away_team_country}
         </div>
-			))}
+			))} */}
+
+      {/* ambil key baru kita mapping */}
+      {Object.keys(matchesByDate).map((date, index) => (
+        <div key={index}>
+
+        </div>
+      ))}
 		</div>
 	);
 }
